4105c30c7ebcf41ac1420614fff52736cd8baad2
SomsakGitHub <somsakkaeworasan@hotmail.com>
2017-11-06 19:16:01 +0700

    Update update_action.php

--- update_action.php
+++ update_action.php
@@ -1,13 +1,17 @@
 <?php
+	// Connect to database
 	require 'config.php';
 
+	// Get data from update.php
 	$Id = $_POST['Id'];
 	$Name = $_POST['Name'];
 	$Data = $_POST['Data'];
 	$Date = $_POST['Date'];
 
+	// SQL update
 	$sql = "UPDATE `testt` SET `Name`='$Name',`Data`='$Data',`Date`='$Date' WHERE testt.Id=$Id";
 
+	// Working
 	$result = $conn->query($sql);
 
 ?>
@@ -18,12 +22,15 @@
 </head>
 <body>
 	<?php
+		// Check working
 		if ($result == 1) {
 			echo "updated";
 		}else{
 			echo "No update";
 		}
 	?>
+	
+	// Link
 	<a href="index.php">Home</a>
 </body>
 </html>
