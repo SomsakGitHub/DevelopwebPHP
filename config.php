da1790bc3f2f9e3c68e56397ce5e3d4935fbe761
SomsakGitHub <somsakkaeworasan@hotmail.com>
2017-11-06 18:44:35 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,12 @@
 <?php
+	// Connect to database
 	$conn = mysqli_connect(
 		"localhost",
 		"root",
 		"",
 		"imformation");
+
+	// Check connect to database
 	// if ($conn) {
 	// 	echo "yesssssss";
 	// }
