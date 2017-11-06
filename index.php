97b748e771682418fbb968a0b8d4bf4e62759f6d
SomsakGitHub <somsakkaeworasan@hotmail.com>
2017-11-06 19:02:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
+	// Connect to database
 	require 'config.php';
 
+	// SQL show all data
 	$sql = $conn->query("SELECT * FROM `testt`");
 
+	// Working
 	$result = $sql->fetch_all();
 	print_r($result)
 	?>
