8c4233a15ac526f5c1650293f795ed680f8cd4c9
SomsakGitHub <somsakkaeworasan@hotmail.com>
2017-11-06 19:07:54 +0700

    Update insert_action.php

--- insert_action.php
+++ insert_action.php
@@ -1,16 +1,18 @@
 <?php
+	// Connect to database
 	require 'config.php';
 
+	// Get data from insert.php
 	$Name = $_POST['Name'];
 	$Data = $_POST['Data'];
 	$Date = $_POST['Date'];
 
+	// Check repeat data 
 	$check = "SELECT * FROM `testt` WHERE Name='$Name' AND Data='$Data' AND Date='$Date'";
-
 	$resultZero = mysqli_query($conn,$check);
-
 	$resultOne = mysqli_num_rows($resultZero);
 
+	// SQL insert data
 	$sql = "INSERT INTO `testt`(`Id`, `Name`, `Data`, `Date`) VALUES (NULL,'$Name','$Data','$Date')";
 
 
@@ -22,12 +24,15 @@
 </head>
 <body>
 	<?php
+		// Check Working
 		if ($resultOne>0) {
 			echo "Have data";
 		}elseif ($result = $conn->query($sql)) {
 			echo "insert success";
 		}
 	?>
+	
+	// Link
 	<a href="index.php">Home</a>
 </body>
 </html>
