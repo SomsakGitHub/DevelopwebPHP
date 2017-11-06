e5b5e0bb3d25557d5bb11264b34a407396733607
SomsakGitHub <somsakkaeworasan@hotmail.com>
2017-11-06 18:56:33 +0700

    Update delete_action.php

--- delete_action.php
+++ delete_action.php
@@ -1,27 +1,33 @@
 <?php
+	// Connect to database
 	require 'config.php';
 
 	$Id = 0;
 	$Id = $_POST['Id'];
 
+	// SQL delete
 	$sql = "DELETE FROM `testt` WHERE testt.Id=$Id";
 
+	// Working
 	$result = $conn->query($sql);
 
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>delaet_action</title>
+	<title>delete_action</title>
 </head>
 <body>
 	<?php
+		// Check Working
 		if ($result == 1) {
 			echo "Delete success";
 		}else{
 			echo "Delete unsuccess";
 		}
 	?>
+	
+	// Link
 	<a href="index.php">Home</a>
 </body>
 </html>
